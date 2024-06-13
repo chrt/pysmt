a64a3d07d4b51ab66a9638e0ee6e748779c0720e
Ruotong Cheng <chrt2000@gmail.com>
2024-06-13 10:35:56 -0400

    Fix SMT-LIB printer for div

--- pysmt/smtlib/printers.py
+++ pysmt/smtlib/printers.py
@@ -93,7 +93,7 @@ class SmtPrinter(TreeWalker):
     def walk_ite(self, formula): return self.walk_nary(formula, "ite")
     def walk_toreal(self, formula): return self.walk_nary(formula, "to_real")
     def walk_div(self, formula): return self.walk_nary(formula, "/")
-    def walk_floordiv(self, formula): return self.walk_nary(formula, "//")
+    def walk_floordiv(self, formula): return self.walk_nary(formula, "div")
     def walk_mod(self, formula): return self.walk_nary(formula, "mod")
     def walk_pow(self, formula): return self.walk_nary(formula, "pow")
     def walk_bv_and(self, formula): return self.walk_nary(formula, "bvand")
@@ -425,7 +425,7 @@ class SmtDagPrinter(DagWalker):
         return self.walk_nary(formula, args, "/")
 
     def walk_floordiv(self, formula, args):
-        return self.walk_nary(formula, args, "//")
+        return self.walk_nary(formula, args, "div")
 
     def walk_mod(self, formula, args):
         return self.walk_nary(formula, args, "mod")
